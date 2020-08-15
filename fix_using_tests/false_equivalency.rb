1d90eda5652e6b94a5cdde2c2d65739663176763
Tej Charfi <charfi@usc.edu>
2020-08-15 16:38:31 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -13,10 +13,12 @@ end
 def selection(num)
   if num = 1
       binding.pry
-     "YUM YUM MUNCH MUNCH MUNCH"
+     return "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
+     return "HAM HAM HAM IN MY TUMMY"
   end
+  
+  
 end
 
 def runner
