46051565269be5d361d93cfa5d5d0dff770d2a0a
Tej Charfi <charfi@usc.edu>
2020-08-15 16:41:29 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,10 +12,8 @@ end
 
 def selection(num)
   if num == 1
-      binding.pry
      return "YUM YUM MUNCH MUNCH MUNCH"
   elsif num == 2
-    binding.pry
      return "HAM HAM HAM IN MY TUMMY"
   end
   
