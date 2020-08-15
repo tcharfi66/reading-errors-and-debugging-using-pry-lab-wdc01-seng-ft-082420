a5b0a7de32c68852294301aa9ce3213f48bf16ac
Tej Charfi <charfi@usc.edu>
2020-08-15 16:43:55 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
 def start_game(player1, player2)
-  binding.pry
+
   "Hello #{player1} & #{player2}"
 end
 
 def play_game(player1,player2)
-  binding.pry
+
   "#{player1} is better than #{player2}"
 end
