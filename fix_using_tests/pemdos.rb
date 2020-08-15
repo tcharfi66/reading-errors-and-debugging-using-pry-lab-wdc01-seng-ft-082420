b61fb0d4395ddc9f4ff3ff5de4b5321d0cae77b9
Tej Charfi <charfi@usc.edu>
2020-08-15 16:58:15 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,12 @@
 require 'pry'
 
 def snake_it_up(string)
+  f_str = ""
   if string[0] == "s"
-    10 * "s" + string
-    binding.pry
+    10.times do 
+      f_str+= "s"
+    end
+    f_str+=string
   else
     string
   end
